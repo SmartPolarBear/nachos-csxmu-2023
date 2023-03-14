f77da21cdc53690498002b0fa8866f96be5dda92
SmartPolarBear <clevercoolbear@outlook.com>
2023-03-14 07:23:19 +0000

    add dllist files

--- code/threads/threadtest.cc
+++ code/threads/threadtest.cc
@@ -11,10 +11,23 @@
 
 #include "copyright.h"
 #include "system.h"
+#include "dllist.h"
 #include "hello.h"
+
+// functions in dllist-driver.cc
+extern void Insert(DLList *L, int N, int whichThread);
+extern void Remove(DLList *L, int N, int whichThread);
 
 // testnum is set in main.cc
 int testnum = 1;
+
+// those are set in main.cc
+int threadnum = 2;	// number of threads, default: 2
+int itemnum = 6;	// number of items, default: 6
+int errorType = 0;	// type of error
+
+// global, pointer to doubly-linked list
+DLList *L;
 
 //----------------------------------------------------------------------
 // SimpleThread
@@ -29,11 +42,23 @@ void
 SimpleThread(int which)
 {
     int num;
-
+    
     for (num = 0; num < 5; num++) {
-        printf("*** thread %d looped %d times\n", which, num);
+	printf("*** thread %d looped %d times\n", which, num);
         currentThread->Yield();
     }
+}
+
+//----------------------------------------------------------------------
+// SimpleThread2
+// 	Manipulate doubly-linked list here.
+//----------------------------------------------------------------------
+
+void
+SimpleThread2(int which)
+{
+    Insert(L, itemnum, which);
+    Remove(L, itemnum, which);
 }
 
 //----------------------------------------------------------------------
@@ -46,12 +71,36 @@ void
 ThreadTest1()
 {
     DEBUG('t', "Entering ThreadTest1");
-
-    Thread* t = new Thread("forked thread");
+	
+    Thread *t = new Thread("forked thread");
 
     t->Fork(SimpleThread, 1);
     SimpleThread(0);
 }
+
+//----------------------------------------------------------------------
+// ThreadTest2
+//  Create a doubly-linked list by calling Insert first, 
+//  and then remove items at the top of the list by those threads.
+//----------------------------------------------------------------------
+
+void
+ThreadTest2()
+{
+    DEBUG('t', "Entering ThreadTest2");
+	
+    // allocate the list
+    L = new DLList();
+	
+    // fork thread
+    for (int i = 1; i < threadnum; ++i)
+    {
+	Thread *t = new Thread("forked thread");
+        t->Fork(SimpleThread2, i);
+    }
+    SimpleThread2(0);
+}
+
 
 //----------------------------------------------------------------------
 // ThreadTest
@@ -64,11 +113,13 @@ ThreadTest()
     hello();
     switch (testnum) {
     case 1:
-        ThreadTest1();
-        break;
+	ThreadTest1();
+	break;
+    case 2:
+	ThreadTest2();	// test for doubly-linked list goes here
+	break;
     default:
-        printf("No test specified.\n");
-        break;
+	printf("No test specified.\n");
+	break;
     }
 }
-
